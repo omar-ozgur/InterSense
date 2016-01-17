36e74f3738503f7d80da24596997ddf099591b08
oozgur <oozgur217@gmail.com>
2016-01-17 03:26:42 -0800

    Added more data

--- scripts/rData.py
+++ scripts/rData.py
@@ -2,6 +2,10 @@ import random
 
 y = [34.065, 34.078]
 x = [-118.455, -118.435]
+Y = [34.045, 34.098]
+X = [-118.475, -118.415]
 
-for i in range(0,20):
-  print(str(random.uniform(y[0], y[1]))+" "+str(random.uniform(x[0], x[1]))+" "+str(random.uniform(0.0, 1000.0))+" \\")
+#for i in range(0,20):
+#  print(str(random.uniform(y[0], y[1]))+" "+str(random.uniform(x[0], x[1]))+" "+str(random.uniform(0.0, 1000.0))+" \\")
+for i in range(0,100):
+  print(str(random.uniform(Y[0], Y[1]))+" "+str(random.uniform(X[0], X[1]))+" "+str(random.uniform(0.0, 1000.0))+" \\")
