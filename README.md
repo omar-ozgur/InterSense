58dc4265d2b6514372ba22d44327a26f1cf96817
oozgur <oozgur217@gmail.com>
2016-01-17 20:52:34 -0800

    Cleaned up files

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # InterSense
-A location based data visualization application to display information about air pollutants within a geographical radius
+An application that allows users to find safe map routes based on air quality data.
+
+## Information
+
+This application was developed during the IDEA Hacks 2016 hackathon at UCLA. My team used an Arduino MQ2 gas sensor to detect air pollutants such as LPG, methane, i-butane, smoke, and alcohol. We mapped this data using the Google Maps API, and used the same API to find routes based on an origin and destination. Our simple algorithm calculates the average pollution amount for a given route, and colors the routes accordingly (green=safe, red=harmful).
+
+## Notes
+
+Although my team only spent a few days developing this product, we believe that it would be benefitial to allow others to use this data for the benefit of everyone. Feel free to download this project and expand on features as you see fit.
---
+++ arduino/pollution.ino
@@ -0,0 +1,240 @@
+#include <SPI.h>
+#include <WiFi101.h>
+
+// Local Network Settings
+char ssid[] = "UCLA_WEB";  // your network SSID (name)
+// your network password
+int keyIndex = 0;             // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+// ThingSpeak Settings
+char thingSpeakAddress[] = "api.thingspeak.com";
+String APIKey = "0YV64N0BRQLWFC5G";             // enter your channel's Write API Key
+const int updateThingSpeakInterval = 2000; // 20 second interval at which to update ThingSpeak
+
+// Variable Setup
+long lastConnectionTime = 0;
+// Initialize Arduino Ethernet Client
+WiFiClient client;
+ 
+/************************Hardware Related Macros************************************/
+#define         MQ_PIN                       (0)     //define which analog input channel you are going to use
+#define         RL_VALUE                     (5)     //define the load resistance on the board, in kilo ohms
+#define         RO_CLEAN_AIR_FACTOR          (9.83)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
+                                                     //which is derived from the chart in datasheet
+ 
+/***********************Software Related Macros************************************/
+#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
+                                                     //cablibration phase
+#define         READ_SAMPLE_INTERVAL         (50)    //define how many samples you are going to take in normal operation
+#define         READ_SAMPLE_TIMES            (5)     //define the time interal(in milisecond) between each samples in 
+                                                     //normal operation
+ 
+/**********************Application Related Macros**********************************/
+#define         GAS_LPG                      (0)
+#define         GAS_CO                       (1)
+#define         GAS_SMOKE                    (2)
+#define         GAS_METHANE                  (3)
+ 
+/*****************************Globals***********************************************/
+float           LPGCurve[3]  =  {2.3,0.21,-0.47};   //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent"
+                                                    //to the original curve. 
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.21), point2: (lg10000, -0.59) 
+float           COCurve[3]  =  {2.3,0.72,-0.34};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.72), point2: (lg10000,  0.15) 
+float           SmokeCurve[3] ={2.3,0.53,-0.44};    //two points are taken from the curve. 
+                                                    //with these two points, a line is formed which is "approximately equivalent" 
+                                                    //to the original curve.
+float           MethaneCurve[3] ={2.3,0.48,-0.34};
+                                                    //data format:{ x, y, slope}; point1: (lg200, 0.53), point2: (lg10000,  -0.22)                                                     
+float           Ro           =  10;                 //Ro is initialized to 10 kilo ohms
+int ledPin = A1;
+void setup()
+
+{
+  pinMode(A1,OUTPUT);
+  Serial.begin(9600);  
+  //UART setup, baudrate = 9600bps
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+   // attempt to connect to Wifi network:
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  Serial.print("Calibrating...\n");                
+  Ro = MQCalibration(MQ_PIN);                       //Calibrating the sensor. Please make sure the sensor is in clean air 
+                                                    //when you perform the calibration                    
+  Serial.print("Calibration is done...\n"); 
+  Serial.print("Ro=");
+  Serial.print(Ro);
+  Serial.print("kohm");
+  Serial.print("\n");
+}
+ 
+void loop()
+{
+   Serial.print("LPG:"); 
+   int gas_lpg = (MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG));
+   Serial.println(gas_lpg);
+   Serial.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_LPG) );
+   Serial.print( "ppm" );
+   Serial.print("    ");   
+   int gas_co = MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CO);
+   Serial.print("CO:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_CO) );
+   Serial.print( "ppm" );
+   Serial.print("    ");  
+   int gas_smoke = (MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_SMOKE));
+   Serial.print("SMOKE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_SMOKE) );
+   Serial.print( "ppm" );
+   Serial.print("\n");
+   int gas_methane = (MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_METHANE));
+   Serial.print("METHANE:"); 
+   Serial.print(MQGetGasPercentage(MQRead(MQ_PIN)/Ro,GAS_METHANE) );
+   Serial.print( "ppm" );
+   Serial.print("\n");
+    if (client.available()) {
+    char c = client.read();
+    Serial.println(c);
+  }
+  Serial.println(client.connect(thingSpeakAddress, 80));
+  if (client.connected()) {
+    updateThingSpeak("field1=" + String(gas_lpg) + "&field2=" + String(gas_co) + "&field3=" + String(gas_smoke) + "&field4=" + String(gas_methane));
+    Serial.println("Sent Data");
+    digitalWrite(ledPin,HIGH);
+    delay(2000);
+    digitalWrite(ledPin,LOW);
+  }
+  delay(10000);
+}
+ void updateThingSpeak(String tsData) {
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + APIKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(tsData.length());
+    client.print("\n\n");
+    client.print(tsData);
+    lastConnectionTime = millis();
+
+    if (client.connected()) {
+      Serial.println("Connecting to ThingSpeak...");
+      Serial.println();
+    }
+  }
+
+
+/****************** MQResistanceCalculation ****************************************
+Input:   raw_adc - raw value read from adc, which represents the voltage
+Output:  the calculated sensor resistance
+Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
+         across the load resistor and its resistance, the resistance of the sensor
+         could be derived.
+************************************************************************************/ 
+float MQResistanceCalculation(int raw_adc)
+{
+  return ( ((float)RL_VALUE*(1023-raw_adc)/raw_adc));
+}
+ 
+/***************************** MQCalibration ****************************************
+Input:   mq_pin - analog channel
+Output:  Ro of the sensor
+Remarks: This function assumes that the sensor is in clean air. It use  
+         MQResistanceCalculation to calculates the sensor resistance in clean air 
+         and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about 
+         10, which differs slightly between different sensors.
+************************************************************************************/ 
+float MQCalibration(int mq_pin)
+{
+  int i;
+  float val=0;
+ 
+  for (i=0;i<CALIBARAION_SAMPLE_TIMES;i++) {            //take multiple samples
+    val += MQResistanceCalculation(analogRead(mq_pin));
+    delay(CALIBRATION_SAMPLE_INTERVAL);
+  }
+  val = val/CALIBARAION_SAMPLE_TIMES;                   //calculate the average value
+ 
+  val = val/RO_CLEAN_AIR_FACTOR;                        //divided by RO_CLEAN_AIR_FACTOR yields the Ro 
+                                                        //according to the chart in the datasheet 
+ 
+  return val; 
+}
+/*****************************  MQRead *********************************************
+Input:   mq_pin - analog channel
+Output:  Rs of the sensor
+Remarks: This function use MQResistanceCalculation to caculate the sensor resistenc (Rs).
+         The Rs changes as the sensor is in the different consentration of the target
+         gas. The sample times and the time interval between samples could be configured
+         by changing the definition of the macros.
+************************************************************************************/ 
+float MQRead(int mq_pin)
+{
+  int i;
+  float rs=0;
+ 
+  for (i=0;i<READ_SAMPLE_TIMES;i++) {
+    rs += MQResistanceCalculation(analogRead(mq_pin));
+    delay(READ_SAMPLE_INTERVAL);
+  }
+ 
+  rs = rs/READ_SAMPLE_TIMES;
+ 
+  return rs;  
+}
+ 
+/*****************************  MQGetGasPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         gas_id      - target gas type
+Output:  ppm of the target gas
+Remarks: This function passes different curves to the MQGetPercentage function which 
+         calculates the ppm (parts per million) of the target gas.
+************************************************************************************/ 
+int MQGetGasPercentage(float rs_ro_ratio, int gas_id)
+{
+  if ( gas_id == GAS_LPG ) {
+     return MQGetPercentage(rs_ro_ratio,LPGCurve);
+  } else if ( gas_id == GAS_CO ) {
+     return MQGetPercentage(rs_ro_ratio,COCurve);
+  } else if ( gas_id == GAS_SMOKE ) {
+     return MQGetPercentage(rs_ro_ratio,SmokeCurve);
+  }  
+  else if (gas_id == GAS_METHANE){
+       return MQGetPercentage(rs_ro_ratio,MethaneCurve);
+  }
+ 
+  return 0;
+}
+ 
+/*****************************  MQGetPercentage **********************************
+Input:   rs_ro_ratio - Rs divided by Ro
+         pcurve      - pointer to the curve of the target gas
+Output:  ppm of the target gas
+Remarks: By using the slope and a point of the line. The x(logarithmic value of ppm) 
+         of the line could be derived if y(rs_ro_ratio) is provided. As it is a 
+         logarithmic coordinate, power of 10 is used to convert the result to non-logarithmic 
+         value.
+************************************************************************************/ 
+int  MQGetPercentage(float rs_ro_ratio, float *pcurve)
+{
+  return (pow(10,( ((log(rs_ro_ratio)-pcurve[1])/pcurve[2]) + pcurve[0])));
+}
